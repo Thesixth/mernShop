454d2e9bfa9628d956fbc7ee54c92f2735fc1c95
Emmanuel Pali <39196449+Thesixth@users.noreply.github.com>
2021-05-19 15:12:32 -0700

    deployment

--- backend/server.js
+++ backend/server.js
@@ -47,9 +47,13 @@ app.use((error, req, res, next) => {
      res.status(500).send({message: error.message })
     })
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-}
+const path = require("path");
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.listen(port, () => {
     console.log(`Server is at http://localhost:${port}`);
