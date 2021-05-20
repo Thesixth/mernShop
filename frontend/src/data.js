6b679ad5c6e1bfd620232954e0092d17296ef27b
Emmanuel Pali <39196449+Thesixth@users.noreply.github.com>
2021-05-20 05:21:06 -0700

    git reset

---
+++ frontend/src/data.js
@@ -0,0 +1,103 @@
+const data = {
+    products: [
+        {
+            _id: '6',
+            name: 'Another One',
+            category: 'speakers',
+            image: '/images/silver-headset.jpg',
+            price: 359,
+            countInStock: 20,
+            brand: 'Khalid'/'s beats',
+            rating: 3.5,
+            numReviews: 10,
+            description: 'Music from the ears of a legendary producer. ',
+        },
+        {
+            _id: '1',
+            name: 'Geo 6',
+            category: 'Watches',
+            image: '/images/black-watch.jpg',
+            price: 299,
+            countInStock: 30,
+            brand: 'Kings',
+            rating: 4,
+            numReviews: 10,
+            description: 'Best in class for the outdoor enthusiast'
+        },
+        {
+            _id: '2',
+            name: 'Bronica',
+            category: 'Cameras',
+            image: '/images/bronica-camera.jpg',
+            price: 1399,
+            countInStock: 10,
+            brand: 'Zenza',
+            rating: 3,
+            numReviews: 10,
+            description: 'Memories are etched in our minds. Life and it\'s worries can wipe some out. capture every moment with this machinery. This item is waterproof'
+        },
+        {
+            _id: '3',
+            name: 'Polar',
+            category: 'Cameras',
+            image: '/images/polaroid.jpg',
+            price: 1999,
+            countInStock: 25,
+            brand: 'Zenza',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Memories are etched in our minds. Life and it\'s worries can wipe some out. capture every moment with this machinery. This item is waterproof'
+        },
+        {
+            _id: '4',
+            name: 'D6700',
+            category: 'Cameras',
+            image: '/images/nikon-camera.jpg',
+            price: 1399,
+            countInStock: 20,
+            brand: 'Zenza',
+            rating: 3.5,
+            numReviews: 10,
+            description: 'Memories are etched in our minds. Life and it\'s worries can wipe some out. capture every moment with this machinery. This item is waterproof'
+        },
+        {
+            _id: '5',
+            name: 'Oculus Go',
+            category: 'VR',
+            image: '/images/oculus-go.jpg',
+            price: 3999,
+            countInStock: 20,
+            brand: 'Oculus',
+            rating: 3,
+            numReviews: 10,
+            description: 'Experience a whole new world right inside your living room.'
+        },
+        
+        {
+            _id: '7',
+            name: 'Geo 5',
+            category: 'watches',
+            image: '/images/white-watch.jpg',
+            price: 310,
+            countInStock: 0,
+            brand: 'Kings',
+            rating: 2.5,
+            numReviews: 10,
+            description: 'Best in class for the outdoor enthusiast'
+        },
+        {
+            _id: '8',
+            name: 'The One',
+            category: 'speakers',
+            image: '/images/black-headset.jpg',
+            price: 310,
+            countInStock: 12,
+            brand: 'Kings',
+            rating:3.5,
+            numReviews: 10,
+            description: 'Music from the ears of a legendary producer. '
+        },
+    ]
+}
+
+export default data;
---
+++ frontend/src/store.js
@@ -0,0 +1,43 @@
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { cartReducer } from './reducers/cartReducer';
+import { orderCreateReducer, orderDetailsReducer, orderPayReducer } from './reducers/orderReducer';
+import { productListReducer, ProductsDetailsReducer } from './reducers/productReducers';
+import { userRegisterReducer, userSigninReducer } from './reducers/userReducer';
+
+const initialState = {
+
+    userSignin:{
+        userInfo: localStorage.getItem('userInfo')
+        ? JSON.parse(localStorage.getItem("userInfo"))
+        : null,
+    },
+
+    cart:{
+        cartItems: localStorage.getItem('cartItems') 
+        ? JSON.parse(localStorage.getItem('cartItems')) 
+        : [],
+        shippingAddress: localStorage.getItem('shippingAddress') 
+        ? JSON.parse(localStorage.getItem('shippingAddress')) 
+        : {},
+        paymentMethod: 'PayPal',
+    },
+};
+const reducer = combineReducers({
+    productList: productListReducer,
+    productDetails: ProductsDetailsReducer,
+    cart: cartReducer,
+    userSignin: userSigninReducer,
+    userRegister: userRegisterReducer,
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
+    orderPay: orderPayReducer,
+})
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducer,
+     initialState,
+      composeEnhancer(applyMiddleware(thunk))
+      );
+
+export default store;
